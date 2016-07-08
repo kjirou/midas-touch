060ead3fb188f59260b7e2f7d624281460eff16e
kjirou <kjirou.web@gmail.com>
2016-07-09 01:48:47 +0900

    Start development

---
+++ src/components/Root.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import pages from './pages';
+
+
+export default class Root extends React.Component {
+
+  _getCurrentPage() {
+    return pages['GAME'];
+    //return pages[this.props.pageId];
+  }
+
+  render() {
+    const Page = this._getCurrentPage();
+    const page = React.createElement(Page, {
+      key: 'GAME',
+      //key: this.props.pageId,
+      root: this.props,
+      //scene: this.props.scene,
+    });
+
+    return (
+      <div className="root">
+        <div className="root__page-container">
+          Hello!
+        </div>
+      </div>
+    );
+  }
+}
