346c63f079f272288b87e92c5e49d6fb66e6563b
kjirou <kjirou.web@gmail.com>
2016-07-09 22:07:38 +0900

    Adjust window size

--- src/components/pages/CanvasPage.js
+++ src/components/pages/CanvasPage.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Page from './Page';
 
 
 export default class CanvasPage extends Page {
 
+  _findCanvasNode() {
+  }
+
+  componentDidMount() {
+    console.log(1111);
+  }
+
   render() {
     return (
       <div className="canvas-page">
-        <div className="canvas-page__canvas" />
+        <div className="canvas-page__canvas .js-canvas-page__canvas" />
       </div>
     );
   }
