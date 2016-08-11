393aaec146fc9b5f4b77bbbe5a30e528df5e36f0
kjirou <kjirou.web@gmail.com>
2016-08-11 20:50:39 +0900

    Add models

---
+++ src/models/AppModel.js
@@ -0,0 +1,13 @@
+import { PAGE_IDS } from '../consts';
+import Model from './Model';
+
+
+export default class AppModel extends Model {
+  constructor() {
+    super();
+
+    this._pageId = PAGE_IDS.CANVAS;
+  }
+
+  get pageId() { return this._pageId; }
+}
